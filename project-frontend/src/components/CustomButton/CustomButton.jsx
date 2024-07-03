29267b10146efa8bd2abdbd48103769e6bdbab59
annahico <annahico@gmail.com>
2024-07-03 20:02:11 +0200

    feat: created home page and custom button

---
+++ project-frontend/src/components/CustomButton/CustomButton.jsx
@@ -0,0 +1,10 @@
+import "./CustomButton.css";
+
+export const CustomButton = ({className, title, functionEmit}) => {
+
+    return (
+        <div className={className} onClick={functionEmit}>
+            {title}
+        </div>
+    )
+}
