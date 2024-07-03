c8287f84cdc3fc7896e0f5808f111f1ff82c2f38
annahico <annahico@gmail.com>
2024-07-03 20:06:08 +0200

    feat: created components folders and customInput

---
+++ project-frontend/src/components/CustomInput/CustomInput.jsx
@@ -0,0 +1,16 @@
+import "./CustomInput.css";
+
+export const CustomInput = ({className, type, placeholder, name, value, onChangeFunction, onBlurFunction}) => {
+
+    return (
+        <input 
+            className={className}
+            type={type}
+            placeholder={placeholder}
+            name={name}
+            value={value}
+            onChange={onChangeFunction}
+            onBlur={onBlurFunction}
+        />
+    )
+}
