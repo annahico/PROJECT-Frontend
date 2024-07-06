71e3d6bfb43125a21b5b789b15f187913948ea76
annahico <annahico@gmail.com>
2024-07-06 09:42:19 +0200

    feat: created appointmentCard

---
+++ project-frontend/src/components/AppointmentCard/AppointmentCard.jsx
@@ -0,0 +1,11 @@
+import "./AppointmentCard.css";
+
+export const AppointmentCard = ({ service_id, appointment_date }) => {
+
+    return (
+        <div className="appointmentCardDesign">
+            <div>{service_id}</div>
+            <div>{appointment_date}</div>
+        </div>
+    )
+}
