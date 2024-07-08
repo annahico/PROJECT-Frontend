1af84c1eff29b5dd245c568ce94f40846f00802a
annahico <annahico@gmail.com>
2024-07-08 19:56:41 +0200

    feat: added superAdminPanel in Header

--- project-frontend/src/common/Header/Header.jsx
+++ project-frontend/src/common/Header/Header.jsx
@@ -2,38 +2,45 @@ import { useNavigate } from "react-router-dom";
 import { CustomLink } from "../../components/CustomLink/CustomLink";
 import "./Header.css";
 
-// Componente Header
 export const Header = () => {
-    const navigate = useNavigate();
-    const passport = JSON.parse(localStorage.getItem("passport"));
+  const navigate = useNavigate();
+  const passport = JSON.parse(localStorage.getItem("passport"));
 
-    // Función para cerrar sesión
-    const logOut = () => {
-        localStorage.removeItem("passport");
-        navigate("/login");
-    };
+  const logOut = () => {
+    localStorage.removeItem("passport");
+    navigate("/login");
+  };
 
-    return (
-        <div className="headerDesign">
-            HEADER
-            <CustomLink title="home" destination="/" />
+  return (
+    <div className="headerDesign">
+      <CustomLink title={"home"} destination={"/"} />
+      <CustomLink title={"services"} destination={"/services"} />
 
-            {passport?.token ? (
-                <div className="menu">
-                    <CustomLink
-                        title={passport?.decodificado?.first_name}
-                        destination="/profile"
-                    />
-                    <div onClick={logOut}>
-                        <CustomLink title="log out" destination="/" />
-                    </div>
-                </div>
-            ) : (
-                <div className="menu">
-                    <CustomLink title="register" destination="/register" />
-                    <CustomLink title="login" destination="/login" />
-                </div>
-            )}
+      {passport?.token ? (
+        <div className="menu">
+          <CustomLink
+            title={passport?.decodificado?.first_name}
+            destination={"/profile"}
+          />
+          <div onClick={logOut}>
+            <CustomLink title={"log out"} destination={"/"} />
+          </div>
         </div>
-    );
+      ) : (
+        <div className="menu">
+          <CustomLink title={"register"} destination={"/register"} />
+          <CustomLink title={"login"} destination={"/login"} />
+        </div>
+      )}
+
+      {passport?.token && passport?.decodificado?.roleName === "super_admin" && (
+        <div className="menu">
+          <CustomLink
+            title={"Super Admin Panel"}
+            destination={"/superadminpanel"}
+          />
+        </div>
+      )}
+    </div>
+  );
 };
