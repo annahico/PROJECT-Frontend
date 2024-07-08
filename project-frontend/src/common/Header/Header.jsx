7c193de9036316749c08ef4de65ddb93fc38d75a
annahico <annahico@gmail.com>
2024-07-08 17:55:59 +0200

    feat: included custonlink in header

--- project-frontend/src/common/Header/Header.jsx
+++ project-frontend/src/common/Header/Header.jsx
@@ -1,9 +1,40 @@
+// import React from 'react';
+import { useNavigate } from "react-router-dom";
+import { CustomLink } from "../../components/CustomLink/CustomLink";
 import "./Header.css";
 
+// Componente Header
 export const Header = () => {
-  return (
-    <div className="headerDesign">
-      HEADER
-    </div>
-  );
+    const navigate = useNavigate();
+    const passport = JSON.parse(localStorage.getItem("passport"));
+
+    // Función para cerrar sesión
+    const logOut = () => {
+        localStorage.removeItem("passport");
+        navigate("/login");
+    };
+
+    return (
+        <div className="headerDesign">
+            HEADER
+            <CustomLink title="home" destination="/" />
+
+            {passport?.token ? (
+                <div className="menu">
+                    <CustomLink
+                        title={passport?.decodificado?.first_name}
+                        destination="/profile"
+                    />
+                    <div onClick={logOut}>
+                        <CustomLink title="log out" destination="/" />
+                    </div>
+                </div>
+            ) : (
+                <div className="menu">
+                    <CustomLink title="register" destination="/register" />
+                    <CustomLink title="login" destination="/login" />
+                </div>
+            )}
+        </div>
+    );
 };
