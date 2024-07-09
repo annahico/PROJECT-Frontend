6a8a06abc9828c79400b1e78dc50fdcffd2b054d
annahico <annahico@gmail.com>
2024-07-09 10:40:54 +0200

    feat: added deleteUsers in ApiCall

--- project-frontend/src/services/apiCalls.js
+++ project-frontend/src/services/apiCalls.js
@@ -101,4 +101,28 @@ export const GetUsers = async (token) => {
     } catch (error) {
         return error;
     }
+}
+
+export const DeleteUsers = async (userId, token) => {
+    const options = {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`
+        }
+    };
+    try {
+        const response = await fetch(`${root}users/${userId}`, options);
+
+        const data = await response.json();
+
+        if (!data.success) {
+            throw new Error(data.message);
+        }
+
+        return data;
+    } catch (error) {
+        return error;
+    }
+
 };
