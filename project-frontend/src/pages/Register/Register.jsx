3b32ea5722c51ea4d8f2984f37c92cf0c5d12a47
annahico <annahico@gmail.com>
2024-07-09 10:51:50 +0200

    feat: added validation in register and superadmin css

--- project-frontend/src/pages/Register/Register.jsx
+++ project-frontend/src/pages/Register/Register.jsx
@@ -4,12 +4,13 @@ import { Header } from "../../common/Header/Header";
 import { CustomButton } from "../../components/CustomButton/CustomButton";
 import { CustomInput } from "../../components/CustomInput/CustomInput";
 import { RegisterUser } from "../../services/apiCalls";
-// import { validate } from "../../utils/functions"; // Asumiendo que validate es una función de utilidad en functions.js
+// import { validame } from "../../utils/functions";
 import "./Register.css";
 
-const Register = () => {
+const datosUser = JSON.parse(localStorage.getItem("passport"));
+
+export const Register = () => {
   const navigate = useNavigate();
-  const datosUser = JSON.parse(localStorage.getItem("passport"));
   const [tokenStorage, setTokenStorage] = useState(datosUser?.token);
   const [user, setUser] = useState({
     first_name: "",
@@ -25,15 +26,14 @@ const Register = () => {
   });
   const [msgError, setMsgError] = useState("");
   const [msgSuccess, setMsgSuccess] = useState("");
+  const [loading, setLoading] = useState(false);
 
-  // Redirige si el usuario ya está logueado
   useEffect(() => {
     if (tokenStorage) {
       navigate("/");
     }
   }, [tokenStorage, navigate]);
 
-  // Maneja el cambio de entrada en los campos
   const inputHandler = (e) => {
     setUser((prevState) => ({
       ...prevState,
@@ -41,33 +41,34 @@ const Register = () => {
     }));
   };
 
-  // Valida los campos de entrada
   const checkError = (e) => {
-    const error = validate(e.target.name, e.target.value); // Asumiendo que validate valida los campos según la función de utilidad
+    const error = validame(e.target.name, e.target.value);
     setUserError((prevState) => ({
       ...prevState,
-      [`${e.target.name}Error`]: error,
+      [e.target.name + "Error"]: error,
     }));
   };
 
-  // Registra al usuario
   const registerMe = async () => {
     try {
-      // Verifica si todos los campos están llenos
-      for (let elemento in user) {
-        if (user[elemento] === "") {
+      for (let field in user) {
+        if (user[field] === "") {
           throw new Error("All fields must be filled");
         }
       }
 
-      // Llama a la API para registrar al usuario
+      setLoading(true);
       const fetched = await RegisterUser(user);
+
       setMsgSuccess(fetched.message);
+
       setTimeout(() => {
+        setLoading(false);
         navigate("/login");
-      }, 1200);
+      }, 2400);
     } catch (error) {
       setMsgError(error.message);
+      setLoading(false);
     }
   };
 
@@ -77,51 +78,43 @@ const Register = () => {
       <div className="registerDesign">
         <pre>{JSON.stringify(user, null, 2)}</pre>
         <CustomInput
-          className={`inputDesign ${
-            userError.first_nameError !== "" ? "inputDesignError" : ""
-          }`}
+          className={`inputDesign ${userError.first_nameError ? "inputDesignError" : ""}`}
           type="text"
           placeholder="First Name"
           name="first_name"
-          value={user.first_name || ""}
-          onChangeFunction={(e) => inputHandler(e)}
-          onBlurFunction={(e) => checkError(e)}
+          value={user.first_name}
+          onChangeFunction={inputHandler}
+          onBlurFunction={checkError}
         />
         <div className="error">{userError.first_nameError}</div>
         <CustomInput
-          className={`inputDesign ${
-            userError.last_nameError !== "" ? "inputDesignError" : ""
-          }`}
+          className={`inputDesign ${userError.last_nameError ? "inputDesignError" : ""}`}
           type="text"
           placeholder="Last Name"
           name="last_name"
-          value={user.last_name || ""}
-          onChangeFunction={(e) => inputHandler(e)}
-          onBlurFunction={(e) => checkError(e)}
+          value={user.last_name}
+          onChangeFunction={inputHandler}
+          onBlurFunction={checkError}
         />
         <div className="error">{userError.last_nameError}</div>
         <CustomInput
-          className={`inputDesign ${
-            userError.emailError !== "" ? "inputDesignError" : ""
-          }`}
+          className={`inputDesign ${userError.emailError ? "inputDesignError" : ""}`}
           type="email"
           placeholder="Email"
           name="email"
-          value={user.email || ""}
-          onChangeFunction={(e) => inputHandler(e)}
-          onBlurFunction={(e) => checkError(e)}
+          value={user.email}
+          onChangeFunction={inputHandler}
+          onBlurFunction={checkError}
         />
         <div className="error">{userError.emailError}</div>
         <CustomInput
-          className={`inputDesign ${
-            userError.password_hashError !== "" ? "inputDesignError" : ""
-          }`}
+          className={`inputDesign ${userError.password_hashError ? "inputDesignError" : ""}`}
           type="password"
           placeholder="Password"
           name="password_hash"
-          value={user.password_hash || ""}
-          onChangeFunction={(e) => inputHandler(e)}
-          onBlurFunction={(e) => checkError(e)}
+          value={user.password_hash}
+          onChangeFunction={inputHandler}
+          onBlurFunction={checkError}
         />
         <div className="error">{userError.password_hashError}</div>
         <CustomButton
@@ -131,9 +124,8 @@ const Register = () => {
         />
         <div className="error">{msgError}</div>
         <div className="successMessage">{msgSuccess}</div>
+        {loading && <span>Loading...</span>}
       </div>
     </>
   );
 };
-
-export default Register;
