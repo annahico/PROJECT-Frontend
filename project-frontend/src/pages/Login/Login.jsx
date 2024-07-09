29e66e22190264c4ea5222da523f8da817231861
annahico <annahico@gmail.com>
2024-07-09 10:48:46 +0200

    feat: added validations in login

--- project-frontend/src/pages/Login/Login.jsx
+++ project-frontend/src/pages/Login/Login.jsx
@@ -2,15 +2,17 @@ import { useEffect, useState } from "react";
 import { decodeToken } from "react-jwt";
 import { useNavigate } from "react-router-dom";
 import { Header } from "../../common/Header/Header";
-import { CustomButton } from "../../components/CustomButton/CustomButton.jsx";
+import { CustomButton } from "../../components/CustomButton/CustomButton";
 import { CustomInput } from "../../components/CustomInput/CustomInput";
 import { LoginUser } from "../../services/apiCalls";
+// import { validame } from "../../utils/functions";
 import "./Login.css";
 
 const datosUser = JSON.parse(localStorage.getItem("passport"));
 
 export const Login = () => {
   const navigate = useNavigate();
+  const [tokenStorage, setTokenStorage] = useState(datosUser?.token);
   const [credenciales, setCredenciales] = useState({
     email: "",
     password_hash: "",
@@ -20,51 +22,63 @@ export const Login = () => {
     password_hashError: "",
   });
   const [msgError, setMsgError] = useState("");
+  const [msgSuccess, setMsgSuccess] = useState("");
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    const tokenStorage = datosUser?.token;
     if (tokenStorage) {
       navigate("/");
     }
-  }, [navigate]);
+  }, [tokenStorage, navigate]);
 
   const inputHandler = (e) => {
-    const { name, value } = e.target;
     setCredenciales((prevState) => ({
       ...prevState,
-      [name]: value,
+      [e.target.name]: e.target.value,
     }));
   };
 
   const checkError = (e) => {
-    const { name, value } = e.target;
-    // const error = validame(name, value);
+    const error = validame(e.target.name, e.target.value);
     setCredencialesError((prevState) => ({
       ...prevState,
-      [`${name}Error`]: error,
+      [e.target.name + "Error"]: error,
     }));
   };
 
   const loginMe = async () => {
     try {
-      for (let key in credenciales) {
-        if (credenciales[key] === "") {
-          throw new Error("Todos los campos tienen que estar rellenos");
+      for (let elemento in credenciales) {
+        if (credenciales[elemento] === "") {
+          throw new Error("All fields must be filled");
         }
       }
+
+      setLoading(true);
       const fetched = await LoginUser(credenciales);
-      const decodificado = decodeToken(fetched.token);
+
+      if (!fetched.success) {
+        setLoading(false);
+        return setMsgError(fetched.message);
+      }
+
+      const decoded = decodeToken(fetched.token);
       const passport = {
         token: fetched.token,
-        decodificado,
+        decoded: decoded,
       };
+
       localStorage.setItem("passport", JSON.stringify(passport));
-      setMsgError(`Hola ${decodificado.first_name}, bienvenido de nuevo`);
+
+      setMsgSuccess(`Hello ${decoded.first_name}, welcome back!`);
+      setLoading(false);
+
       setTimeout(() => {
         navigate("/profile");
       }, 2000);
     } catch (error) {
       setMsgError(error.message);
+      setLoading(false);
     }
   };
 
@@ -73,35 +87,33 @@ export const Login = () => {
       <Header />
       <div className="loginDesign">
         <CustomInput
-          className={`inputDesign ${
-            credencialesError.emailError ? "inputDesignError" : ""
-          }`}
-          type="email"
-          placeholder="email"
-          name="email"
+          className={`inputDesign ${credencialesError.emailError ? "inputDesignError" : ""}`}
+          type={"email"}
+          placeholder={"email"}
+          name={"email"}
           value={credenciales.email}
-          onChange={inputHandler}
-          onBlur={checkError}
+          onChangeFunction={inputHandler}
+          onBlurFunction={checkError}
         />
         <div className="error">{credencialesError.emailError}</div>
         <CustomInput
-          className={`inputDesign ${
-            credencialesError.password_hashError ? "inputDesignError" : ""
-          }`}
-          type="password"
-          placeholder="password"
-          name="password_hash"
+          className={`inputDesign ${credencialesError.password_hashError ? "inputDesignError" : ""}`}
+          type={"password"}
+          placeholder={"password"}
+          name={"password_hash"}
           value={credenciales.password_hash}
-          onChange={inputHandler}
-          onBlur={checkError}
+          onChangeFunction={inputHandler}
+          onBlurFunction={checkError}
         />
         <div className="error">{credencialesError.password_hashError}</div>
         <CustomButton
-          className="customButtonDesign"
-          title="Login"
+          className={"customButtonDesign"}
+          title={"Login"}
           functionEmit={loginMe}
         />
         <div className="error">{msgError}</div>
+        <div className="success">{msgSuccess}</div>
+        {loading && <span>Loading...</span>}
       </div>
     </>
   );
