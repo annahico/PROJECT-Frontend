25e0238fc769dfc96fcf0bd5b573456bcaf41cd9
annahico <annahico@gmail.com>
2024-07-10 12:42:14 +0200

    update: updated home folder

--- project-frontend/src/pages/Home/Home.jsx
+++ project-frontend/src/pages/Home/Home.jsx
@@ -15,10 +15,10 @@ export const Home = () => {
                             Located in the heart of the city, InkSoul is more than just a tattoo and piercing studio; it's a sanctuary of body art where creativity meets technical skill to transform ideas into lasting masterpieces.
                             At InkSoul, we are dedicated to providing a unique and personalized experience for each client. We believe that every tattoo and piercing tells a story, and our goal is to help you tell yours in the most authentic and artistic way possible.
                         </div>
-                        <img src="../../../img/imgProyect/diseñoTatto4.png" alt="InkSoul Studio" className="descriptionImage" />
+                        <img src="" alt="InkSoul Studio" className="descriptionImage" />
                     </div>
                     <div className="section">
-                        <img src="../../../img/imgProyect/tattoMaquina.png" alt="InkSoul Studio" className="descriptionImage" />
+                        <img src="" alt="InkSoul Studio" className="descriptionImage" />
                         <div className="descriptionDesign">
                             We offer a wide range of services, from custom tattoos and artistic piercings to touch-ups and cover-ups. Additionally, we provide free consultations to discuss your ideas, answer your questions, and ensure you are completely satisfied with the design before starting the tattoo or piercing process.
                         </div>
@@ -27,7 +27,7 @@ export const Home = () => {
                         <div className="descriptionDesign">
                             We use only the best inks, needles, and materials available on the market to ensure high-quality, long-lasting results. Moreover, we keep up with the latest trends and techniques in the world of tattooing and piercing to offer you the best services possible.
                         </div>
-                        <img src="../../../img/imgProyect/tattoDiseño3.png" alt="InkSoul Studio" className="descriptionImage" />
+                        <img src="" alt="InkSoul Studio" className="descriptionImage" />
                     </div>
                 </div>
             </div>
