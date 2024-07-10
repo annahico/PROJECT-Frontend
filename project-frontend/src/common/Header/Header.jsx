7f7dfca3bfac0bed92e4de415fd0cb6c22e229e1
annahico <annahico@gmail.com>
2024-07-10 11:07:00 +0200

    update: update header section

--- project-frontend/src/common/Header/Header.jsx
+++ project-frontend/src/common/Header/Header.jsx
@@ -1,46 +1,75 @@
+import { useEffect, useState } from 'react';
 import { useNavigate } from "react-router-dom";
-import { CustomLink } from "../../components/CustomLink/CustomLink";
+import { CustomLink } from "../CustomLink/CustomLink";
 import "./Header.css";
 
 export const Header = () => {
-  const navigate = useNavigate();
-  const passport = JSON.parse(localStorage.getItem("passport"));
+    const navigate = useNavigate();
+    const passport = JSON.parse(localStorage.getItem("passport"));
+    const [headerScroll, setHeaderScroll] = useState(false);
 
-  const logOut = () => {
-    localStorage.removeItem("passport");
-    navigate("/login");
-  };
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrollTop = window.scrollY || document.documentElement.scrollTop;
+            if (scrollTop > 0) {
+                setHeaderScroll(true);
+            } else {
+                setHeaderScroll(false);
+            }
+        };
+        window.addEventListener("scroll", handleScroll);
 
-  return (
-    <div className="headerDesign">
-      <CustomLink title={"home"} destination={"/"} />
-      <CustomLink title={"services"} destination={"/services"} />
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
 
-      {passport?.token ? (
-        <div className="menu">
-          <CustomLink
-            title={passport?.decodificado?.first_name}
-            destination={"/profile"}
-          />
-          <div onClick={logOut}>
-            <CustomLink title={"log out"} destination={"/"} />
-          </div>
+    const logOut = () => {
+        localStorage.removeItem("passport");
+        navigate("/login");
+    };
+
+    return (
+        <div className={headerScroll ? "headerDesign scrolled" : "headerDesign"}>
+            <CustomLink
+                title={"InkSoul"}
+                destination={"/"}
+            />
+            <CustomLink
+                title={"Services"}
+                destination={"/services"}
+            />
+
+            {passport?.token
+                ? (
+                    <div className="authMenu">
+                        <CustomLink
+                            title={passport?.decoded?.firstName}
+                            destination={"/profile"}
+                        />
+                        <div>
+                            <CustomLink title={"My Appointments"} destination={"/appointments"} />
+                        </div>
+                        <div>
+                            <CustomLink title={"Request Appointment"} destination={"/newappointments"} />
+                        </div>
+                        {passport?.decoded?.name === 'super_admin' && (
+                            <div>
+                                <CustomLink title={"Users"} destination={"/users"} />
+                            </div>
+                        )}
+                        <div onClick={logOut}>
+                            <CustomLink title={"Log Out"} destination={"/"} />
+                        </div>
+                    </div>
+                )
+                : (
+                    <div className="authMenu">
+                        <CustomLink title={"Account"} destination={"/login"} />
+                        <CustomLink title={"Register"} destination={"/register"} />
+                    </div>
+                )
+            }
         </div>
-      ) : (
-        <div className="menu">
-          <CustomLink title={"register"} destination={"/register"} />
-          <CustomLink title={"login"} destination={"/login"} />
-        </div>
-      )}
-
-      {passport?.token && passport?.decodificado?.roleName === "super_admin" && (
-        <div className="menu">
-          <CustomLink
-            title={"Super Admin Panel"}
-            destination={"/superadminpanel"}
-          />
-        </div>
-      )}
-    </div>
-  );
+    );
 };
