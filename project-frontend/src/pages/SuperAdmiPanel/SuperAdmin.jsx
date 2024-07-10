0d0b29bd1e904e3068d65a277c00253bf24f5ee7
annahico <annahico@gmail.com>
2024-07-10 10:38:58 +0200

    fix: fixed mistakes in superadmin

--- project-frontend/src/pages/SuperAdmiPanel/SuperAdmin.jsx
+++ project-frontend/src/pages/SuperAdmiPanel/SuperAdmin.jsx
@@ -1,40 +1,75 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { Header } from "../../common/Header/Header";
+import { AppointmentCard } from "../../components/AppointmentCard/AppointmentCard";
 import { CustomButton } from "../../components/CustomButton/CustomButton";
+import { CustomLink } from "../../components/CustomLink/CustomLink";
 import { ProfileCard } from "../../components/ProfileCard/ProfileCard";
-import { DeleteUsers, GetUsers } from "../../services/apiCalls";
+import { DeleteUsers, GetAppointmentsUsersProfile, GetUsers, SeeUsersProfile } from "../../services/apiCalls";
 import "./SuperAdmin.css";
 
 export const SuperAdminPanel = () => {
     const [users, setUsers] = useState([]);
+    const [userProfile, setUserProfile] = useState({
+        id: "",
+        first_name: "",
+        last_name: "",
+        email: "",
+    });
+    const [userAppointmentProfile, setUserAppointmentProfile] = useState([]);
     const datosUser = JSON.parse(localStorage.getItem("passport"));
-    const [tokenStorage, setTokenStorage] = useState(datosUser?.token);
+    const [tokenStorage] = useState(datosUser?.token);
     const [msgError, setMsgError] = useState("");
     const [msgSuccess, setMsgSuccess] = useState("");
 
+    const bringData = useCallback(async () => {
+        try {
+            const fetched = await GetUsers(tokenStorage);
+            setUsers(fetched.data);
+        } catch (error) {
+            console.log(error);
+        }
+    }, [tokenStorage]);
+
     useEffect(() => {
-        const fetchData = async () => {
-            if (users.length === 0) {
-                try {
-                    const fetched = await GetUsers(tokenStorage);
-                    setUsers(fetched.data);
-                } catch (error) {
-                    console.log(error);
-                }
-            }
-        };
-        fetchData();
-    }, [users, tokenStorage]);
+        if (users.length === 0) {
+            bringData();
+        }
+    }, [users, bringData]);
 
     const deleteUser = async (userId) => {
         try {
             const fetched = await DeleteUsers(userId, tokenStorage);
             if (!fetched.success) {
-                setMsgError(fetched.message);
-                return;
+                return setMsgError(fetched.message);
             }
             setMsgSuccess(fetched.message);
-            setUsers(users.filter(user => user.id !== userId));
+            bringData();
+        } catch (error) {
+            setMsgError(error);
+        }
+    };
+
+    const getUsersAppointments = async (userId) => {
+        try {
+            const fetched = await GetAppointmentsUsersProfile(userId, tokenStorage);
+            if (!fetched.success) {
+                return setMsgError(fetched.message);
+            }
+            setUserAppointmentProfile(fetched.data);
+            setMsgSuccess(fetched.message);
+        } catch (error) {
+            setMsgError(error);
+        }
+    };
+
+    const seeUserProfile = async (userId) => {
+        try {
+            const fetched = await SeeUsersProfile(userId, tokenStorage);
+            if (!fetched.success) {
+                return setMsgError(fetched.message);
+            }
+            setUserProfile(fetched.data);
+            setMsgSuccess(fetched.message);
         } catch (error) {
             setMsgError(error);
         }
@@ -48,7 +83,22 @@ export const SuperAdminPanel = () => {
                     <div className="superAdminPanelDesign">
                         <div className="error">{msgError}</div>
                         <div className="success">{msgSuccess}</div>
-                        {users.map(user => (
+                        {userProfile.id && (
+                            <>
+                                <ProfileCard
+                                    user_id={userProfile.id}
+                                    first_name={userProfile.first_name}
+                                    last_name={userProfile.last_name}
+                                    email={userProfile.email}
+                                />
+                                <CustomButton
+                                    className={"customButtonDesign"}
+                                    title={`Ver citas de ${userProfile.first_name}`}
+                                    functionEmit={() => getUsersAppointments(userProfile.id)}
+                                />
+                            </>
+                        )}
+                        {users.map((user) => (
                             <div key={user.id}>
                                 <ProfileCard
                                     first_name={user.first_name}
@@ -57,17 +107,36 @@ export const SuperAdminPanel = () => {
                                 />
                                 <CustomButton
                                     className={"customButtonDesign"}
-                                    title={"Borrar usuario"}
+                                    title={`Borrar a ${user.first_name}`}
                                     functionEmit={() => deleteUser(user.id)}
+                                />
+                                <CustomButton
+                                    className={"customButtonDesign"}
+                                    title={`Ver perfil de ${user.first_name}`}
+                                    functionEmit={() => seeUserProfile(user.id)}
                                 />
                             </div>
                         ))}
                     </div>
                 ) : (
                     <div className="superAdminPanelDesign">
-                        <p>Los servicios estan viniendo</p>
+                        <p>Los servicios están viniendo</p>
                     </div>
                 )}
+
+                {userAppointmentProfile.length > 0 &&
+                    userAppointmentProfile.map((appointment) => (
+                        <AppointmentCard
+                            key={appointment.id}
+                            service_id={appointment.service_id}
+                            appointment_date={appointment.appointment_date}
+                        />
+                    ))}
+
+                <CustomLink
+                    destination={"/superadminpanelservices"}
+                    title={"Ver servicios de admin"}
+                />
             </div>
         </>
     );
