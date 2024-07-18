39a3e89e97717beb0c0e683c347ba14b02dd855d
annahico <annahico@gmail.com>
2024-07-18 10:41:14 +0200

    update: updated localstorage to usecontext in appointments, feat authprovider

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App.jsx';
-import { AuthProvider } from './context/UseContext.jsx'; // Importar AuthProvider en lugar de UserContext
-import './index.css';
+import App from './App';
+import { AuthProvider } from './context/UseContext.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
-  </React.StrictMode>,
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <AuthProvider>
+                <App />
+            </AuthProvider>
+        </BrowserRouter>
+    </React.StrictMode>
 );
