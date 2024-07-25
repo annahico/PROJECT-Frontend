b58b69e4fe733395b300c2595bae4b41ece42779
annahico <annahico@gmail.com>
2024-07-25 20:31:31 +0200

    update: updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,15 @@
-import { Route, Routes } from "react-router-dom";
-import "./App.css";
-import Admin from "./pages/Admin/Admin";
-import Appointments from "./pages/Appointments/Appointments";
-import Artists from "./pages/Artists/Artists";
-import Home from "./pages/Home/Home";
-import Login from "./pages/Login/Login";
-import Profile from "./pages/Profile/Profile";
-import Register from "./pages/Register/Register";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Footer } from "./common/Footer/Footer";
+import { Header } from "./common/Header/Header";
+import { Body } from "./pages/Body/Body";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="login" element={<Login />} />
-      <Route path="artists" element={<Artists />} />
-      <Route path="register" element={<Register />} />
-      <Route path="profile" element={<Profile />} />
-      <Route path="admin" element={<Admin />} />
-      <Route path="appointment" element={<Appointments />} />
-    </Routes>
+    <>
+      <Header />
+      <Body />
+      <Footer />
+    </>
   );
 }
 
